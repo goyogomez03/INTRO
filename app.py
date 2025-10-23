7754dbd7183a9fa8b04ec8c1c28db5aa94240d88
goyogomez03 <goyogomez03@gmail.com>
2025-10-22 23:50:48 -0500

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title(" Mi Primera App!!")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write("Facilmente puedo realizar backend y frontend.")
-image = Image.open('perro')
+image = Image.open('perro.jpg')
 
 st.image(image, caption='Interfaces multimodales')
 

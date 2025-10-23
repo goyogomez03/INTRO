a256a0137599495c29c18c84d667544a38de6d54
goyogomez03 <goyogomez03@gmail.com>
2025-10-22 23:55:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title(" Mi Primera App!!")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write("Facilmente puedo realizar backend y frontend.")
-image = Image.open('perro.jpg')
+image = Image.open('perro.jpeg')
 
 st.image(image, caption='perro.jpeg')
 
